5de960e2479753b6d63fb0d73d75612763453d03
Krystenae Dennis <krystenaedennis@gmail.com>
2026-06-07 20:04:00 -0400

    Add files via upload
    employee records and html file

---
+++ cc_5.js
@@ -0,0 +1,19 @@
+// Employee Records
+
+let employees = [
+    {
+        name: "John Smith",
+        hourlyRate: 20,
+        hoursWorked: 38
+    },
+    {
+        name: "Sarah Johnson",
+        hourlyRate: 25,
+        hoursWorked: 45
+    },
+    {
+        name: "Michael Brown",
+        hourlyRate: 18,
+        hoursWorked: 50
+    }
+];
